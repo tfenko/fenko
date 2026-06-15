67f8c04cf539f9297b1532967f97a75e5d0b20ab
tfenko <tarasfenko13@gmail.com>
2026-06-15 14:33:10 +0300

    Update ThemeContext.tsx

--- app/components/ThemeContext.tsx
+++ app/components/ThemeContext.tsx
@@ -1,9 +1,15 @@
-// components/ThemeContext.tsx
+'use client';
+
 import { ThemeProvider } from 'next-themes';
 
 export function ThemeContext({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+    <ThemeProvider 
+      attribute="class" 
+      defaultTheme="dark" 
+      forcedTheme="dark" // Це змушує сайт завжди ігнорувати світлу тему при першому завантаженні
+      enableSystem={false}
+    >
       {children}
     </ThemeProvider>
   );
