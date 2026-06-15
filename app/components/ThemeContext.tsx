eb2b67f9240fff7b7444a6ebfa5ad9a208708b60
tfenko <tarasfenko13@gmail.com>
2026-06-15 13:01:51 +0300

    Create ThemeContext.tsx

---
+++ app/components/ThemeContext.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { ThemeProvider } from 'next-themes';
+
+export function ThemeContext({ children }: { children: React.ReactNode }) {
+  return <ThemeProvider attribute="class" defaultTheme="dark">{children}</ThemeProvider>;
+}
