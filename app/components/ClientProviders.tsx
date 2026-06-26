bed7afa08d854ff2e352e1efdfb4ad220a07be14
tfenko <tarasfenko13@gmail.com>
2026-06-26 23:21:31 +0300

    refactor: clean up ClientProviders and remove defunct FloatingNotes dependency

--- app/components/ClientProviders.tsx
+++ app/components/ClientProviders.tsx
@@ -2,24 +2,13 @@
 
 import dynamic from 'next/dynamic';
 
-// Імпортуємо тільки ті компоненти, які мають залишатися
+// Імпортуємо тільки необхідний функціонал
 const SmoothScroll = dynamic(() => import('./SmoothScroll'), { ssr: false });
-const FloatingNotes = dynamic(() => import('./FloatingNotes'), { ssr: false });
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      {/* Фонові елементи, які не заважають взаємодії 
-        та не блокують стандартний курсор 
-      */}
-      <FloatingNotes />
-      
-      {/* SmoothScroll залишається для плавного ефекту, 
-        але тепер він не конфліктує з кастомним курсором
-      */}
-      <SmoothScroll>
-        {children}
-      </SmoothScroll>
-    </>
+    <SmoothScroll>
+      {children}
+    </SmoothScroll>
   );
 }
