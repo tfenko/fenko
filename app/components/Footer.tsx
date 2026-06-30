6e8ab1f89c401e8f42e438e65786b96222f1995b
tfenko <tarasfenko13@gmail.com>
2026-06-30 21:38:50 +0300

    «update»

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,8 +4,8 @@ import { motion, useMotionValue, useTransform, animate, useInView } from 'framer
 import { useEffect, useRef } from 'react';
 
 export default function Footer() {
-  const TOTAL_STREAMS = 12797;
-  const CURRENT_DATE = "June 29, 2026";
+  const TOTAL_STREAMS = 13085;
+  const CURRENT_DATE = "June 30, 2026";
 
   const count = useMotionValue(0);
   const rounded = useTransform(count, (latest) => Math.round(latest).toLocaleString());
